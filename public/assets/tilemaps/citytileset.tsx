b44a1a356f56184d517341ae74edd555a6128141
Daniel Garcia <danielg_97@yahoo.com>
2019-04-30 17:23:13 -0400

    Updated level tiles

--- public/assets/tilemaps/citytileset.tsx
+++ public/assets/tilemaps/citytileset.tsx
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="building" tilewidth="128" tileheight="128" tilecount="4" columns="2">
- <image source="../spritesheets/tiles/test.png" width="256" height="256"/>
- <tile id="0">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="1">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="2">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
+<tileset version="1.2" tiledversion="1.2.3" name="citytileset" tilewidth="128" tileheight="128" tilecount="10" columns="5">
+ <image source="../spritesheets/tilespritesheet.png" width="640" height="256"/>
  <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
