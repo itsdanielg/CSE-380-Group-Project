512685538e5263a3f5b368d70ab7c9fe000880cd
Daniel Garcia <danielg_97@yahoo.com>
2019-05-10 05:20:35 -0400

    Moved constants to new JS file

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,3 @@
-var WIDTH = 1280;
-var HEIGHT = 720;
-var SCALE = WIDTH/3000;
-var SCALE_ENLARGE = SCALE + 0.1;
-var DEPTH = {
-    BACKGROUND: 0,
-    COLLISION: 1,
-    ITEM: 2,
-    SPRITE: 3,
-    HEALTHBAR: 4,
-    OVERLAY: 5,
-    OVERLAYTEXT: 6,
-}
-var FONT = 'Lato';
-var WORDWRAPWIDTH = 260;
-var GOODQUESTFILL = "#7CFC00";
-var BADQUESTFILL = "#FF2D00";
-
 var config = {
     type: Phaser.AUTO,
     width: WIDTH,
