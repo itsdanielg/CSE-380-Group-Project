a9d23c9a5c1879625d24c41a4969e86e34621b52
Daniel Garcia <danielg_97@yahoo.com>
2019-05-13 21:45:19 -0400

    Added map editing and more tiles

--- public/assets/tilemaps/citytileset.tsx
+++ public/assets/tilemaps/citytileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="citytileset" tilewidth="128" tileheight="128" tilecount="10" columns="5">
- <image source="../spritesheets/tilespritesheet.png" width="640" height="256"/>
+<tileset version="1.2" tiledversion="1.2.3" name="citytileset" tilewidth="128" tileheight="128" tilecount="15" columns="5">
+ <image source="../spritesheets/tilespritesheet.png" width="640" height="384"/>
  <tile id="3">
   <properties>
    <property name="collides" type="bool" value="true"/>
@@ -11,12 +11,42 @@
    <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
+ <tile id="5">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="8">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="9">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
